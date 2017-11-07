cf52c6f5a93af12ba6f900a00288a89d6d73d3cb
jentest <jentest@agent.zabbix.com>
2017-11-07 17:51:48 +0530

    added test HTTPS

--- Hello12.java
+++ Hello12.java
@@ -3,7 +3,7 @@ public class Hello12{
 	public static void main(String[] args){
 	
 	System.out.println("Hello 123333333");
-	System.out.println("Hello 123333333 jentest1");
+	System.out.println("Hello 123333333 jentest1111111111111111111111111111111111111111111111111111111111111111111");
 	}
 
 }
