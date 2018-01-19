a9a542212ceb24e8a8d281f16adefb39bb16e179
jitendrasingh11 <mailtojitendra11@gmail.com>
2018-01-19 16:21:37 +0530

    lal

--- Hello1.java
+++ Hello1.java
@@ -1,7 +1,7 @@
 public class Hello1{
  public static void main(String[] args){
 for(int i=1; i<=5; i++){
-System.out.println("helloq");
+System.out.println("hgggggggelloq");
                 }
 
         }
